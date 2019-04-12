9c58a8c0937055858062d456179562427404a6a2
Arata Jingu <guujin0120@gmail.com>
2019-04-12 14:11:33 +0900

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <pcl/visualization/cloud_viewer.h>
+#include <iostream>
+#include <pcl/io/io.h>
+#include <pcl/io/pcd_io.h>
+
+int user_data;
+
+void viewerOneOff(pcl::visualization::PCLVisualizer& viewer)
+{
+	viewer.setBackgroundColor(1.0, 0.5, 1.0);
+	pcl::PointXYZ o;
+	o.x = 1.0;
+	o.y = 0;
+	o.z = 0;
+	viewer.addSphere(o, 0.25, "sphere", 0);
+	std::cout << "i only run once" << std::endl;
+
+}
+
+void
+viewerPsycho(pcl::visualization::PCLVisualizer& viewer)
+{
+	static unsigned count = 0;
+	std::stringstream ss;
+	ss << "Once per viewer loop: " << count++;
+	viewer.removeShape("text", 0);
+	viewer.addText(ss.str(), 200, 300, "text", 0);
+	user_data++;
+}
+
+int main()
+{
+	pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGBA>);
+
+	pcl::visualization::CloudViewer viewer("Cloud Viewer");
+	viewer.showCloud(cloud);
+
+	//This will only get called once
+	viewer.runOnVisualizationThreadOnce(viewerOneOff);
+
+	//This will get called once per visualization iteration
+	viewer.runOnVisualizationThread(viewerPsycho);
+	while (!viewer.wasStopped())
+	{
+		user_data++;
+	}
+	return 0;
+}
